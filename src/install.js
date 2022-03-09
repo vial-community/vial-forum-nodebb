795959b409468067b2b1e76df48c1271d6a4a30c
Zhivko Angelov <zhivko.angelov.0@gmail.com>
2022-03-09 15:15:15 +0200

    Enable social login with Google & Facebook

--- src/install.js
+++ src/install.js
@@ -495,7 +495,9 @@ async function enableDefaultPlugins() {
 		'nodebb-plugin-emoji-android',
 		'nodebb-plugin-embed',
 		'nodebb-plugin-share-post-icons',
-		'nodebb-plugin-s3-uploads-digitalocean'
+		'nodebb-plugin-s3-uploads-digitalocean',
+		'nodebb-plugin-sso-facebook',
+		'nodebb-plugin-sso-google'
 	];
 	let customDefaults = nconf.get('defaultplugins') || nconf.get('defaultPlugins');
 
